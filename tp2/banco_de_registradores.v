c72bd7b058953c54600b48899bc16243a4194152
henriquebh <henriquebhorizonte@gmail.com>
2017-07-06 12:11:26 -0300

    ultimos arquivos - henrique

---
+++ tp2/banco_de_registradores.v
@@ -0,0 +1,208 @@
+module banco_de_registradores(
+
+	input					br_in_clk,
+	input[4:0]			br_in_rs,
+	input[4:0]			br_in_rt,
+	input[4:0]			br_in_rd,
+	input[31:0]			br_in_data,
+	
+	input					br_in_w_en,
+	input					br_in_rst,
+	
+	output reg[31:0]	br_out_R_rs,
+	output reg[31:0]	br_out_R_rt
+);
+
+
+
+reg [31:0] zero;
+reg [31:0] at;
+reg [31:0] v0;
+reg [31:0] v1;
+reg [31:0] a0;
+reg [31:0] a1;
+reg [31:0] a2;
+reg [31:0] a3;
+reg [31:0] t0;
+reg [31:0] t1;
+reg [31:0] t2;
+reg [31:0] t3;
+reg [31:0] t4;
+reg [31:0] t5;
+reg [31:0] t6;
+reg [31:0] t7;
+reg [31:0] s0;
+reg [31:0] s1;
+reg [31:0] s2;
+reg [31:0] s3;
+reg [31:0] s4;
+reg [31:0] s5;
+reg [31:0] s6;
+reg [31:0] s7;
+reg [31:0] t8;
+reg [31:0] t9;
+reg [31:0] k0;
+reg [31:0] k1;
+reg [31:0] gp;
+reg [31:0] sp;
+reg [31:0] fp;
+reg [31:0] ra;
+
+//always@(br_in_rs,br_in_rt,br_in_clk)begin
+		
+always@(posedge br_in_clk)begin
+
+	case(br_in_rs[4:0])
+		5'b00000:br_out_R_rs = zero;
+		5'b00001:br_out_R_rs = at;
+		5'b00010:br_out_R_rs = v0;
+		5'b00011:br_out_R_rs = v1;
+		5'b00100:br_out_R_rs = a0;
+		5'b00101:br_out_R_rs = a1;
+		5'b00110:br_out_R_rs = a2;
+		5'b00111:br_out_R_rs = a3;
+		5'b01000:br_out_R_rs = t0;
+		5'b01001:br_out_R_rs = t1;
+		5'b01010:br_out_R_rs = t2;
+		5'b01011:br_out_R_rs = t3;
+		5'b01100:br_out_R_rs = t4;
+		5'b01101:br_out_R_rs = t5;
+		5'b01110:br_out_R_rs = t6;
+		5'b01111:br_out_R_rs = t7;
+		5'b10000:br_out_R_rs = s0;
+		5'b10001:br_out_R_rs = s1;
+		5'b10010:br_out_R_rs = s2;
+		5'b10011:br_out_R_rs = s3;
+		5'b10100:br_out_R_rs = s4;
+		5'b10101:br_out_R_rs = s5;
+		5'b10110:br_out_R_rs = s6;
+		5'b10111:br_out_R_rs = s7;
+		5'b11000:br_out_R_rs = t8;
+		5'b11001:br_out_R_rs = t9;
+		5'b11010:br_out_R_rs = k0;
+		5'b11011:br_out_R_rs = k1;
+		5'b11100:br_out_R_rs = gp;
+		5'b11101:br_out_R_rs = sp;
+		5'b11110:br_out_R_rs = fp;
+		5'b11111:br_out_R_rs = ra;
+		default: br_out_R_rs = 32'b0;
+	endcase
+	
+	case(br_in_rt[4:0])
+		5'b00000:br_out_R_rt = zero;
+		5'b00001:br_out_R_rt = at;
+		5'b00010:br_out_R_rt = v0;
+		5'b00011:br_out_R_rt = v1;
+		5'b00100:br_out_R_rt = a0;
+		5'b00101:br_out_R_rt = a1;
+		5'b00110:br_out_R_rt = a2;
+		5'b00111:br_out_R_rt = a3;
+		5'b01000:br_out_R_rt = t0;
+		5'b01001:br_out_R_rt = t1;
+		5'b01010:br_out_R_rt = t2;
+		5'b01011:br_out_R_rt = t3;
+		5'b01100:br_out_R_rt = t4;
+		5'b01101:br_out_R_rt = t5;
+		5'b01110:br_out_R_rt = t6;
+		5'b01111:br_out_R_rt = t7;
+		5'b10000:br_out_R_rt = s0;
+		5'b10001:br_out_R_rt = s1;
+		5'b10010:br_out_R_rt = s2;
+		5'b10011:br_out_R_rt = s3;
+		5'b10100:br_out_R_rt = s4;
+		5'b10101:br_out_R_rt = s5;
+		5'b10110:br_out_R_rt = s6;
+		5'b10111:br_out_R_rt = s7;
+		5'b11000:br_out_R_rt = t8;
+		5'b11001:br_out_R_rt = t9;
+		5'b11010:br_out_R_rt = k0;
+		5'b11011:br_out_R_rt = k1;
+		5'b11100:br_out_R_rt = gp;
+		5'b11101:br_out_R_rt = sp;
+		5'b11110:br_out_R_rt = fp;
+		5'b11111:br_out_R_rt = ra;
+		default: br_out_R_rt = 32'b0;
+	endcase
+	
+end
+	
+always@(posedge br_in_clk)begin
+	if(br_in_rst == 1'b0)
+	begin
+		zero = 32'b0;
+		at = 32'b0;
+		v0 = 32'b0;
+		v1 = 32'b0;
+		a0 = 32'b0;
+		a1 = 32'b0;
+		a2 = 32'b0;
+		a3 = 32'b0;
+		t0 = 32'b0;
+		t1 = 32'b0;
+		t2 = 32'b0;
+		t3 = 32'b0;
+		t4 = 32'b0;
+		t5 = 32'b0;
+		t6 = 32'b0;
+		t7 = 32'b0;
+		s0 = 32'b0;
+		s1 = 32'b0;
+		s2 = 32'b0;
+		s3 = 32'b0;
+		s4 = 32'b0;
+		s5 = 32'b0;
+		s6 = 32'b0;
+		s7 = 32'b0;
+		t8 = 32'b0;
+		t9 = 32'b0;
+		k0 = 32'b0;
+		k1 = 32'b0;
+		gp = 32'b0;
+		sp = 32'b0;
+		fp = 32'b0;
+		ra = 32'b0;
+	end else
+	if(br_in_w_en == 1'b1)
+	begin
+		case(br_in_rd[4:0])
+		5'b00000:zero = br_in_data;
+		5'b00001:at = br_in_data;
+		5'b00010:v0 = br_in_data;
+		5'b00011:v1 = br_in_data;
+		5'b00100:a0 = br_in_data;
+		5'b00101:a1 = br_in_data;
+		5'b00110:a2 = br_in_data;
+		5'b00111:a3 = br_in_data;
+		5'b01000:t0 = br_in_data;
+		5'b01001:t1 = br_in_data;
+		5'b01010:t2 = br_in_data;
+		5'b01011:t3 = br_in_data;
+		5'b01100:t4 = br_in_data;
+		5'b01101:t5 = br_in_data;
+		5'b01110:t6 = br_in_data;
+		5'b01111:t7 = br_in_data;
+		5'b10000:s0 = br_in_data;
+		5'b10001:s1 = br_in_data;
+		5'b10010:s2 = br_in_data;
+		5'b10011:s3 = br_in_data;
+		5'b10100:s4 = br_in_data;
+		5'b10101:s5 = br_in_data;
+		5'b10110:s6 = br_in_data;
+		5'b10111:s7 = br_in_data;
+		5'b11000:t8 = br_in_data;
+		5'b11001:t9 = br_in_data;
+		5'b11010:k0 = br_in_data;
+		5'b11011:k1 = br_in_data;
+		5'b11100:gp = br_in_data;
+		5'b11101:sp = br_in_data;
+		5'b11110:fp = br_in_data;
+		5'b11111:ra = br_in_data;
+		default: br_out_R_rt = 32'b0;
+	endcase
+	end
+		
+end
+	
+	
+endmodule
+	
---
+++ tp2/novo_mips_pipeline.v
@@ -0,0 +1,321 @@
+
+
+module novo_mips_pipeline(
+
+	input				CLOCK_50,// Para a placa
+	input[3:0]		KEY, // Para a placa
+	input[17:0]		SW, // Para a placa
+	output[8:0]		LEDG, // Para a placa
+	output[0:6]		HEX0, // Para a placa
+	output[0:6]		HEX1, // Para a placa
+	output[0:6]		HEX2, // Para a placa
+	output[0:6]		HEX3, // Para a placa
+	output[0:6]		HEX4, // Para a placa
+	output[0:6]		HEX5, // Para a placa
+	output[0:6]		HEX6, // Para a placa
+	output[0:6]		HEX7 // Para a placa
+
+);
+
+
+
+
+reg halt;
+
+
+reg [31:0] clk; // contador utilizado para deixar a frequência do clock na placa mais lento
+
+//PC -> enderecos no arquivo vao de zero a 1023, entao tem 10 bits
+reg [9:0] PC;
+
+// registradores intermediarios no pipeline
+reg [31:0] IR_1;
+reg [31:0] IR_2;
+reg [31:0] IR_3;
+reg [31:0] IR_4;
+
+//guarda a saida da ULA 
+reg [31:0] saida_ula_1; //para ser usada no quarto estagio
+reg [31:0] saida_ula_2; //para ser usada no quinto estagio
+
+//primeiro operando rs
+reg [31:0] A;
+
+//segundo operando rt
+reg [31:0] B_1;
+reg [31:0] B_2; //rt encaminhado para o quarto estagio (necessario para o store)
+
+wire [31:0] out_mem_inst; //saida da memoria de instrucao (o IR_1 recebe esse valor)
+wire [31:0] out_mem_data; //saida da memoria de dados
+
+
+wire [31:0] dado_lido_1; //ligado ao IR[25:21]
+wire [31:0] dado_lido_2; //ligado ao IR[20:16]
+
+// write enable: o valor é 1 quando for pra escrever nos registradores
+// por causa da existencia desse trem, nao precisa fazer nada no estagio 5
+wire signal_br_in_w_en;
+wire signal_wren;
+
+// valor do registrador de destino
+wire [4:0] signal_rd;
+
+
+
+	mem_inst mem_i(
+	
+	.address(PC),
+	.clock(clk[25]),
+	.q(out_mem_inst)
+	);
+	
+	
+	
+	//MEM DATA
+	
+	mem_data mem_d(
+	
+	.address(saida_ula_1[9:0]), 
+	.clock(clk[25]),
+	.data(B_2), 
+	.wren(signal_wren),
+	.q(out_mem_data));
+	
+	
+	
+
+	banco_de_registradores br(
+
+	.br_in_clk(clk[25]),
+	.br_in_rst(KEY[0]),
+	.br_in_rs(IR_1[25:21]),
+	.br_in_rt(IR_1[20:16]),
+	.br_in_rd(signal_rd),
+	.br_in_data(saida_ula_2),
+	.br_in_w_en(signal_br_in_w_en),
+	.br_out_R_rs(dado_lido_1),
+	.br_out_R_rt(dado_lido_2)
+	);
+	
+	
+	
+	//////////DECODER////////////
+	
+	displayDecoder DP7_0(
+	.entrada(saida_ula_1[3:0]),
+	.saida(HEX0)); // para a placa
+	
+	displayDecoder DP7_1(
+	.entrada(saida_ula_2[7:4]),
+	.saida(HEX1)); // para a placa
+	
+	assign LEDG[0] = clk[25];
+	
+	
+	
+	always@(posedge clk[25])begin
+		clk = clk + 1;
+	end
+	
+	//////////DECODER////////////
+	
+	
+	
+assign signal_wren	=	(IR_3[31:26] == 6'b101011) ? 1 : 0; //data mem
+
+// se for uma operacao do tipo R, o segundo registrador passado é o de destino
+// se nao (tipo I), é o segundo
+assign signal_rd	=	(IR_4[31:26] == 6'b000000) ? IR_4[15:11] : IR_4[20:16];
+
+// para as instrucoes add, sub, addi e load, sempre que elas estiverem no
+// ultimo estagio, o write enable vai ser 1. caso contrario, ele e zero,
+// o resto do trabalho pra escrever ja e feito no banco de regs.
+assign signal_br_in_w_en = ((IR_4[31:26] == 6'b000000 && (IR_4[5:0] == 6'b100000 || IR_4[5:0] == 6'b100010)) || IR_4[31:26] == 6'b001000 || IR_4[31:26] == 6'b100011)  ? 1'b1 : 1'b0;
+	
+// nosso loop de execucao (FICA TUDO NO MESMO POSEDGE MESMO)
+	always@(posedge clk[25])begin
+	
+		if(KEY[0] == 1'b0)
+		begin
+			
+			PC <= 10'b0;
+			IR_1 <= 32'b0;
+			IR_2 <= 32'b0;
+			IR_3 <= 32'b0;
+			IR_4 <= 32'b0;
+			A <= 32'b0;
+			B_1 <= 32'b0;
+			B_2 <= 32'b0;
+			saida_ula_1 <= 32'b0;
+			saida_ula_2 <= 32'b0;
+				
+			halt <= 1'b1;
+		end
+		
+		else
+		begin
+			
+			if(halt == 1'b1)
+			begin
+				halt <= 1'b0;
+				PC <= PC + 1;
+			end
+			
+			else
+			begin
+				
+				PC <= PC + 1;
+				IR_1 <= out_mem_inst;
+
+		
+		
+				
+				
+				////////////////////////////
+				//leitura do banco de registradores / decode
+				////////////////////////////
+				A <= dado_lido_1;
+				B_1 <= dado_lido_2;
+				IR_2 <= IR_1;
+				
+				// INSTRUCAO NOP
+		
+		if(IR_1[31:0] != 32'b0 && IR_1[31:26] == 6'b000010 && IR_1[31:26] == 6'b101011)
+		begin
+		
+		if(IR_1[31:26] == 6'b000000 || IR_1[31:26] == 6'b000100)begin
+
+		if(
+		IR_1[25:21] == IR_2[25:21] || IR_1[25:21] == IR_3[25:21] ||
+      IR_1[20:16] == IR_2[20:16] || IR_1[20:16] == IR_3[20:16] ||
+		IR_1[25:21] == IR_4[25:21] ||
+      IR_1[20:16] == IR_4[20:16]
+		) begin
+				
+				IR_2[31:0] <= 32'b0;
+				PC <= PC;
+			end
+			
+		end
+		
+		if(IR_1[31:26] == 6'b001000 || IR_1[31:26] == 6'b100011 )begin
+		
+		if(
+		IR_1[25:21] == IR_2[25:21] || IR_1[25:21] == IR_3[25:21] ||
+		IR_1[25:21] == IR_4[25:21]
+		) begin
+				
+				IR_2[31:0] <= 32'b0;
+				PC <= PC;
+			end
+			
+		end
+		
+		end
+		
+				////////////////////////////
+				//leitura do banco de registradores / decode
+				////////////////////////////
+				
+				
+				
+				
+				
+				
+				
+				////////////////////////////
+				//Excute
+				////////////////////////////
+				if(IR_2[31:26] == 6'b000000)begin
+					if(IR_2[5:0] == 6'b100000)begin //add
+						saida_ula_1 <= A + B_1;
+					end
+					if(IR_2[5:0] == 6'b100010)begin //sub
+						saida_ula_1 <= A - B_1;
+					end
+				end
+				
+				if(IR_2[31:26] == 6'b001000)begin //addi
+				
+					saida_ula_1 <= A + IR_2[15:0];
+					
+				end
+				
+				if(IR_2[31:26] == 6'b000100)begin //beq
+				
+					//saida_ula_1 <= A + B_1;
+					if(A == B_1)
+					begin
+					
+					PC <= PC + 4 + IR_2[15:0];
+					
+					end
+					
+				end
+				
+				if(IR_2[31:26] == 6'b000010)begin //jump
+				
+				PC <= PC + 4 + IR_2[25:0];
+					
+				end
+				
+				if(IR_2[31:26] == 6'b100011)begin //lw
+				
+					saida_ula_1 <= A + IR_2[15:0];
+					
+				end
+				
+				if(IR_2[31:26] == 6'b101011)begin //sw
+				
+					saida_ula_1 <= A + IR_2[15:0];
+					
+				end
+				
+				B_2 <= B_1; //para SW
+				IR_3 <= IR_2;
+				////////////////////////////
+				//Excute
+				////////////////////////////
+				
+				
+				
+				
+				
+				
+				
+				
+				////////////////////////////
+				//Memory
+				////////////////////////////
+				saida_ula_2 <= saida_ula_1;
+				if(IR_3[31:26] == 6'b100011)begin //lw
+				
+					saida_ula_2 <= out_mem_data;
+					
+				end
+				IR_4 <= IR_3;
+				////////////////////////////
+				//Memory
+				////////////////////////////
+				
+				
+				
+				
+				
+				
+				////////////////////////////
+				//WB
+				////////////////////////////
+				
+				////////////////////////////
+				//WB
+				////////////////////////////
+				
+			end
+		end
+		
+	end
+	
+	
+
+endmodule
